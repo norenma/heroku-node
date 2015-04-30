7acfcd935df412a4ae1b81d74c36441cf211280b
norenma <markus.a.noren@gmail.com>
2015-04-30 11:46:19 +0200

    #2

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index');
+    //  res.render('index');
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Our app is running on http://localhost:' + port);
 });
