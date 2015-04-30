f40691c3ab6460c5f9a2620a24dd7fd3dc3125be
norenma <markus.a.noren@gmail.com>
2015-04-30 11:56:27 +0200

    #4

--- server.js
+++ server.js
@@ -21,3 +21,26 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
     console.log('Our app is running on http://localhost:' + port);
 });
+
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function (req, res) {
+    res.json({
+        message: 'hooray! welcome to our api!'
+    });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
